b3aba85f05e75a6957fab4d20f728b10ec74b10e
Jeffrey Torres <torres.jeff@hotmail.com>
2016-03-18 09:53:31 -0500

    Fix al tablero, clonacion de matriz y no por referencia

--- src/Player1.java
+++ src/Player1.java
@@ -6,7 +6,7 @@ public class Player1 implements ISolver
 {
     /**
      * Implementacion de la funcion solve() de la interfaz ISolver. Ver comentarios de la interfaz.
-     * El jugador es responsable de hacerle un "swap" a los valores de la matriz para actualizar el tablero segun los movimientos que quiere que se hagan.
+     * El jugador es responsable de hacerle un "swap" a los valores de la matriz para actualizar el tablero segun los movimientos que se van haciendo.
      * @param tablero matriz que indica el orden actual de las fichas.
      * @param n tamaño de UNA dimension del tablero.
      * @return Lista que indica las fichas que se deben mover EN ORDEN. Todos los movimientos deben ser legales.
@@ -16,7 +16,6 @@ public class Player1 implements ISolver
     public ArrayList<Integer> solve(int[][] tablero, int n)
     {
         System.out.println("Tablero dado: ");
-        //System.out.println("" + tablero[0][2]);
         for (int i = 0; i < n; ++i)
         {
             for (int j = 0; j < n; ++j)
@@ -26,14 +25,41 @@ public class Player1 implements ISolver
             System.out.println("");
         }
         
-        ArrayList<Integer> movimientos = new ArrayList<>();
         
-        //TODO: reemplazar este codigo por la implementacion verdadera.
+        ArrayList<Integer> movimientos = new ArrayList<>();
+        int aux;
+        
+        //TODO: REEMPLAZAR ESTE CODIGO POR LA IMPLEMENTACION VERDADERA.
         
         //Estamos moviendo las fichas de la ultima columna
+        //1er movimiento
         movimientos.add(tablero[n-2][n-1]);
+        aux = tablero[n-2][n-1];
+        tablero[n-2][n-1] = -1;
+        tablero[n-1][n-1] = aux; //Actualizamos las fichas del tablero, cambiando el espacio vacio por la ficha que acabamos de mover. Esto es una función swap.
+        
+        //2do movimiento
         movimientos.add(tablero[n-3][n-1]);
+        aux = tablero[n-3][n-1];
+        tablero[n-3][n-1] = -1;
+        tablero[n-2][n-1] = aux;
+        
+        //3er movimiento
         movimientos.add(tablero[n-4][n-1]);
+        aux = tablero[n-4][n-1];
+        tablero[n-4][n-1] = -1;
+        tablero[n-3][n-1] = aux;
+        
+        
+        System.out.println("Tablero final: ");
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                System.out.print(tablero[i][j] + "\t");
+            }
+            System.out.println("");
+        }
         
         return movimientos;
     }
