3ef6b883e5534aebcc81703b43e65451e30dde0d
Jeffrey Torres <torres.jeff@hotmail.com>
2016-03-17 23:15:04 -0500

    bug fixes and improvements

--- src/ThreadButton.java
+++ src/ThreadButton.java
@@ -6,15 +6,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author ASUS
+ * Thread que permite pausar entre los movimientos de los botones del Taquin.
  */
 public class ThreadButton extends Thread implements Runnable{
     
@@ -91,7 +86,7 @@ public class ThreadButton extends Thread implements Runnable{
                 }
             }
             try {
-                Thread.sleep(3000);
+                Thread.sleep(700);
                 i++;
             } catch (InterruptedException ex) {
                 Logger.getLogger(ThreadButton.class.getName()).log(Level.SEVERE, null, ex);
